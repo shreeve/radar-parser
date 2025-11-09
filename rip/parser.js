2ff9c1c2b3b3a4c3cf4a4b61d36c4d8ac40cb799
Steve Shreeve <steve.shreeve@gmail.com>
2025-11-09 01:24:19 -0700

    Updates from Rip

--- rip/parser.js
+++ rip/parser.js
@@ -898,7 +898,7 @@ parseSplat() {const $$1 = this._match('...');
     // Parse base case
     switch (this.la.kind) {    case 'IDENTIFIER':
       {
-      const $$1 = this.parseIdentifier();
+      const $$1 = this._match('IDENTIFIER');
       base = $$1;;
       break;
       }
@@ -1224,7 +1224,7 @@ parseSuper() {
       {
       const $$1 = this._match('SUPER');
       const $$2 = this._match('.');
-      const $$3 = this.parseProperty();
+      const $$3 = this._match('PROPERTY');
       return [".", "super", $$3];
       }      default:      this._error(['SUPER'], "Invalid Super");    }
   }
@@ -1234,14 +1234,14 @@ parseMetaProperty() {
       {
       const $$1 = this._match('NEW_TARGET');
       const $$2 = this._match('.');
-      const $$3 = this.parseProperty();
+      const $$3 = this._match('PROPERTY');
       return [".", "new", $$3];
       }
     case 'IMPORT_META':
       {
       const $$1 = this._match('IMPORT_META');
       const $$2 = this._match('.');
-      const $$3 = this.parseProperty();
+      const $$3 = this._match('PROPERTY');
       return [".", "import", $$3];
       }      default:      this._error(['NEW_TARGET', 'IMPORT_META'], "Invalid MetaProperty");    }
   }
@@ -1534,12 +1534,12 @@ parseImportSpecifierListTail() {
     this._error(['IDENTIFIER', 'DEFAULT'], "Invalid ImportSpecifier");
   }
 
-parseImportDefaultSpecifier() {const $$1 = this.parseIdentifier();
+parseImportDefaultSpecifier() {const $$1 = this._match('IDENTIFIER');
       return $$1;  }
 
 parseImportNamespaceSpecifier() {const $$1 = this._match('IMPORT_ALL');
       const $$2 = this._match('AS');
-      const $$3 = this.parseIdentifier();
+      const $$3 = this._match('IDENTIFIER');
       return ["*", $$3];  }
 
   parseExport() {
@@ -1807,7 +1807,7 @@ parseThis() {
   }
 
 parseThisProperty() {const $$1 = this._match('@');
-      const $$2 = this.parseProperty();
+      const $$2 = this._match('PROPERTY');
       return [".", "this", $$2];  }
 
   parseArray() {
@@ -3406,11 +3406,7 @@ parseOperation() {
 
 parseDoIife() {const $$1 = this._match('DO_IIFE');
       const $$2 = this.parseCode();
-      return ["do-iife", $$2];  }
-
-parse$accept() {const $$1 = this.parseRoot();
-      const $$2 = this._match('$end');
-      return [$$1, $$2];  }}
+      return ["do-iife", $$2];  }}
 
 const parser = new Parser();
 
